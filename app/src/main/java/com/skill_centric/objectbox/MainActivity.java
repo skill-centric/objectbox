f5d18e5c6505aba404002daf3e5aa752d8da20c1
skill-centric <info@skill-centric.com>
2018-09-04 07:25:50 +0300

    3 students are stored.

--- app/src/main/java/com/skill_centric/objectbox/MainActivity.java
+++ app/src/main/java/com/skill_centric/objectbox/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.Arrays;
 import java.util.GregorianCalendar;
 
 import io.objectbox.Box;
@@ -15,6 +16,8 @@ import io.objectbox.BoxStore;
 public class MainActivity extends AppCompatActivity {
 
     private BoxStore boxStore;
+    private Box<Student> studentBox;
+    private Box<Address> addressBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +25,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         boxStore = ((TheApp) getApplication()).getBoxStore();
-        final Box<Student> studentBox = boxStore.boxFor(Student.class);
+        studentBox = boxStore.boxFor(Student.class);
+        addressBox = boxStore.boxFor(Address.class);
 
         studentBox.removeAll();
-
         storeStudents();
-
         Toast.makeText(this, "Students are created!", Toast.LENGTH_SHORT).show();
-
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -41,12 +42,51 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // NOTE: in production code persistence related operations
+    // must be done in the background thread!
     private void storeStudents() {
 
+        // Create student1
         Student student1 = new Student(0, "James", "Jackson", 87.5,
                 Gender.MALE, new GregorianCalendar(1999, 5, 6).getTime());
 
+        Address address11 = new Address("Russia", "Moscow", "Lenin St.",
+                123, "623500");
 
+        Address address12 = new Address("Russia", "St. Petersburg", "Broad St.",
+                543, "432890");
 
+        student1.getAddresses().add(address11);
+        student1.getAddresses().add(address12);
+
+        // Create student2
+        Student student2 = new Student(0, "Jane", "Sommers", 95.4,
+                Gender.FEMALE, new GregorianCalendar(2000, 1, 4).getTime());
+
+        Address address21 = new Address("UK", "London", "Abchurch Lane",
+                12, "AD232B");
+
+        Address address22 = new Address("UK", "London", "Idol Lane",
+                43, "UI3232L");
+
+        student2.getAddresses().add(address21);
+        student2.getAddresses().add(address22);
+
+        // Create student3
+        Student student3 = new Student(0, "Mike", "Nickson", 82.3,
+                Gender.MALE, new GregorianCalendar(1998, 3, 8).getTime());
+
+        Address address31 = new Address("USA", "New York", "Sheffield Ave.",
+                77, "14075");
+
+        Address address32 = new Address("USA", "New York", "Baker Ave",
+                605, "11758");
+
+        student3.getAddresses().add(address31);
+        student3.getAddresses().add(address32);
+
+        // Store the addresses and the students
+        addressBox.put(Arrays.asList(address11, address12, address21, address22, address31, address32));
+        studentBox.put(Arrays.asList(student1, student2, student3));
     }
 }
