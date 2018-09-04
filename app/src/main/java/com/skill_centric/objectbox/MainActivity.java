fede0881f31fe6692c127034f4dfe66df2740511
skill-centric <info@skill-centric.com>
2018-09-04 07:07:28 +0300

    student1 is created in storeStudents method

--- app/src/main/java/com/skill_centric/objectbox/MainActivity.java
+++ app/src/main/java/com/skill_centric/objectbox/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.GregorianCalendar;
 
 import io.objectbox.Box;
 import io.objectbox.BoxStore;
@@ -21,23 +24,29 @@ public class MainActivity extends AppCompatActivity {
         boxStore = ((TheApp) getApplication()).getBoxStore();
         final Box<Student> studentBox = boxStore.boxFor(Student.class);
 
+        studentBox.removeAll();
+
+        storeStudents();
+
+        Toast.makeText(this, "Students are created!", Toast.LENGTH_SHORT).show();
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                studentBox.removeAll();
 
-                storeStudents();
-
-                Snackbar.make(view, "Students are created!", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
             }
         });
     }
 
     private void storeStudents() {
 
-        // TODO
+        Student student1 = new Student(0, "James", "Jackson", 87.5,
+                Gender.MALE, new GregorianCalendar(1999, 5, 6).getTime());
+
+
+
     }
 }
